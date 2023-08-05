2ccb6f9af11e75416a5bb3b37611f473b27e4c45
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-08-05 10:06:16 +0545

    Touch screen functionality added

--- src/index.js
+++ src/index.js
@@ -47,4 +47,33 @@ refreshBtn.addEventListener('click', async () => {
   });
 });
 
+refreshBtn.addEventListener('touchstart', async (event) => {
+  event.preventDefault();
+  const scores = await refreshScores(gameId);
+  const heading = document.getElementById('heading');
+  heading.classList.add('bigger');
+  const resultTable = document.getElementById('ul');
+  let child = resultTable.lastElementChild;
+  while (child) {
+    resultTable.removeChild(child);
+    child = resultTable.lastElementChild;
+  }
+
+  scores.forEach((individualUser) => {
+    const li = document.createElement('li');
+    li.className = 'listItem';
+    li.innerHTML = `${individualUser.user} : ${individualUser.score}`;
+
+    const isEvenIndex = resultTable.children.length % 2 === 0;
+
+    if (isEvenIndex) {
+      li.style.backgroundColor = 'white';
+    } else {
+      li.style.backgroundColor = '#cdcbcb';
+    }
+
+    resultTable.appendChild(li);
+  });
+});
+
 export default gameId;
